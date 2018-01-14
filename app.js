326e6d5861806b2d6f75950e69198cd7f4c43fc6
Muhammad Haekal <93haekal@gmail.com>
2018-01-14 12:08:09 +0700

    build using object inside array

--- app.js
+++ app.js
@@ -1,36 +1,42 @@
-var addressBook = {
-  1: {
+var addressBook = [
+  {
     firstName: "firstname1",
     lastName: "lastame2",
     age: 22,
     phoneNum: "phonenum1",
     address: "address1"
   },
-  2: {
+  {
     firstName: "firstname2",
     lastName: "fristame2",
     age: 23,
     phoneNum: "phonenum2",
     address: "address2"
   },
-  3: {
+  {
     firstName: "firstname1",
     lastName: "lastame3",
-    age: 22,
+    age: 24,
     phoneNum: "phonenum3",
     address: "address3"
   },
-}
-var tempObj = []
+  {
+    firstName: "firstname4",
+    lastName: "lastame4",
+    age: 24,
+    phoneNum: "phonenum4",
+    address: "address4"
+  }
+]
+console.log(addressBook);
 
 // ----------------------------- create -----------------------------------------------
-function addContact(inputFirst_name = null, inputLastName = null, inputAge = null, inputPhoneNum = null, inputAddress = null) {
+function addContact(firstName = null, lastName = null, age = null, phoneNum = null, address = null) {
   // start input validation
 
   // end of input validation
-  var newKey = parseInt(Object.keys(addressBook)[Object.keys(addressBook).length - 1]) + 1
-  var newValue = {inputFirst_name,inputLastName,inputAge,inputPhoneNum,inputAddress}
-  addressBook[newKey] = newValue
+  var newValue = {firstName,lastName,age,phoneNum,address}
+  addressBook.push(newValue)
 }
 
 // ----------------------------- read -----------------------------------------------
@@ -40,15 +46,17 @@ function displayAllContact(){
 
 function searchContactBy(properties ,value){
   var arrKeys = []
-  for (var key in addressBook){
-    if(addressBook[key][properties] === value){
-      arrKeys.push(key)
+
+  for(i=0;i<addressBook.length;i++){
+    if(addressBook[i][properties] === value){
+      arrKeys.push(i)
     }
   }
-
+  // console.log(arrKeys);
   for (i=0;i<arrKeys.length;i++){
     console.log(addressBook[arrKeys[i]])
   }
+
 }
 // ----------------------------- update -----------------------------------------------
 
@@ -58,25 +66,25 @@ function editContactByKey(key,properties,value){
 }
 
 // ----------------------------- delete -----------------------------------------------
-function deleteContactByFirstName(inputFirstname){
-  var intKey = parseInt(Object.keys(addressBook).find(key => addressBook[key].firstName == inputFirstname))
-  delete addressBook[intKey]
-}
-
 function deleteContactByKey(inputkey){
-  delete addressBook[inputkey]
+  addressBook.splice(0,inputkey);
+  console.log(addressBook);
 }
 
 function deleteContact(properties,value){
   var arrKeys = []
-  for (var key in addressBook){
-    if(addressBook[key][properties] === value){
-      arrKeys.push(key)
+
+  for(i=0;i<addressBook.length;i++){
+    if(addressBook[i][properties] === value){
+      arrKeys.push(i)
     }
   }
-  for (i=0;i<arrKeys.length;i++){
-    delete addressBook[arrKeys[i]]
+  arrKeys = arrKeys.sort().reverse();
+  // console.log(arrKeys);
+  for (i=arrKeys.length;i>0;i--){
+    addressBook.splice(arrKeys[i],1);
   }
+  console.log(addressBook);
 }
 
 
